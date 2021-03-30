50726804b7f24352da28bf05ecb243d152756ebc
Arsalan Khattak <me@khattak.dev>
2021-03-30 18:08:17 +0500

    feat: connect to db

--- app.js
+++ app.js
@@ -1,11 +1,24 @@
 const express = require("express");
+const expressLayouts = require("express-ejs-layouts");
+const mongoose = require("mongoose");
 const indexRoutes = require("./routes/index");
 const authRoutes = require("./routes/auth");
 
-const PORT = process.env.port || 3000;
 const app = express();
+
+app.use(expressLayouts);
+app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: false }));
 
 app.use("/", indexRoutes);
 app.use("/auth", authRoutes);
 
+const PORT = process.env.port || 3000;
+
+mongoose
+  .connect("", { useNewUrlParser: true })
+  .then((res) => {
+    console.log("MongoDB connected");
+  })
+  .catch((err) => console.log(err));
 app.listen(PORT, console.log("Server started..."));
--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const router = express.Router();
 
-router.get("/login", (req, res) => res.send("Login Page"));
+router.get("/login", (req, res) => res.render("login"));
 
-router.get("/register", (req, res) => res.send("Register Page"));
+router.get("/register", (req, res) => res.render("register"));
 
 module.exports = router;
