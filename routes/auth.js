7f2d1f5f5fb6bb0cc93e82fe0c2d76df5b1eb11a
Arsalan Khattak <me@khattak.dev>
2021-03-30 20:06:40 +0500

    feat: add ejs files

--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-router.get("/login", (req, res) => res.render("login"));
-
-router.get("/register", (req, res) => res.render("register"));
+router.get("/register", (req, res) => res.render("register", { errors: [] }));
+router.get("/login", (req, res) => res.render("login", { errors: [] }));
 
 module.exports = router;
